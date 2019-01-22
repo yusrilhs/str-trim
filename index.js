7ec99eaaf09f769f061758746d289857230c00dc
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2019-01-22 10:05:53 +0700

    Fix Issue #1 and change method

--- index.js
+++ index.js
@@ -12,19 +12,19 @@
 
     /**
      * Escape regex sepecial characters
-     * @param  {String} chars 
-     * @return {String}       
+     * @param  {String} chars
+     * @return {String}
      */
     function escapeRegexChars(chars) {
         return chars
-                .replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1')
+                .replace(/[.*+?^${}()|[\]\\<>]/g, '\\$&')
                 .replace(/\x08/g, '\\x08');
     };
 
     /**
      * Trim Character on left and right
-     * @param  {String} chars 
-     * @return {String}      
+     * @param  {String} chars
+     * @return {String}
      */
     String.prototype.trim = function(chars) {
         var pattern;
@@ -33,36 +33,36 @@
             pattern = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
         } else {
             chars = escapeRegexChars(chars);
-            pattern = new RegExp('^['+ chars +']+|['+ chars +']+$', 'g');
+            pattern = new RegExp('^('+ chars +'+)|('+ chars +'+)$', 'g');
         }
-    
+
         return this.replace(pattern, '');
     };
 
     /**
      * Trim Character on left side
-     * @param  {String} chars 
-     * @return {String}      
+     * @param  {String} chars
+     * @return {String}
      */
     String.prototype.ltrim = function(chars) {
-        var pattern = (typeof chars !== 'string') ? 
-                        /^[\s\uFEFF\xA0]+/g : 
-                        new RegExp('^['+ escapeRegexChars(chars) +']+', 'g');
+        var pattern = (typeof chars !== 'string') ?
+                        /^[\s\uFEFF\xA0]+/g :
+                        new RegExp('^'+ escapeRegexChars(chars) +'+', 'g');
 
         return this.replace(pattern, '');
     }
 
     /**
      * Trim Character on right side
-     * @param  {String} chars 
-     * @return {String}      
+     * @param  {String} chars
+     * @return {String}
      */
     String.prototype.rtrim = function(chars) {
-        var pattern = (typeof chars !== 'string') ? 
-                        /[\s\uFEFF\xA0]+$/g : 
-                        new RegExp('['+ escapeRegexChars(chars) +']+$', 'g');
+        var pattern = (typeof chars !== 'string') ?
+                        /[\s\uFEFF\xA0]+$/g :
+                        new RegExp(escapeRegexChars(chars) +'+$', 'g');
 
         return this.replace(pattern, '');
     }
-    
+
 }));
